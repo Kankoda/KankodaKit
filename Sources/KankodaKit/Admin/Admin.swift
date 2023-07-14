4cf0e6694590ad2c3bf60d851e725574b9f601bf
Daniel Saidi <daniel.saidi@gmail.com>
2023-07-14 06:46:25 +0200

    Add admin namespace

---
+++ Sources/KankodaKit/Admin/Admin.swift
@@ -0,0 +1,14 @@
+//
+//  Admin.swift
+//  KankodaKit
+//
+//  Created by Daniel Saidi on 2023-06-27.
+//  Copyright © 2022-2023 Daniel Saidi. All rights reserved.
+//
+
+import Foundation
+
+/**
+ This namespace contains admin-specific types.
+ */
+public struct Admin {}
---
+++ Sources/KankodaKit/PackageInfo.swift
@@ -0,0 +1,18 @@
+//
+//  PackageInfo.swift
+//  KankodaKit
+//
+//  Created by Daniel Saidi on 2023-07-14.
+//  Copyright © 2023 Daniel Saidi. All rights reserved.
+//
+
+import Foundation
+
+/**
+ This class can be used to configure the package.
+ */
+public class PackageInfo {
+    
+    /// The prefix to apply before app storage keys.
+    public static var appStoragePrefix = "com.kankoda."
+}

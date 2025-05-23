98ef84a179617815945de6d7ed841f5c00970a3a
Daniel Saidi <daniel.saidi@gmail.com>
2025-05-23 14:54:14 +0200

    Add StandardButtons dependency

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         .iOS(.v17),
         .macOS(.v14),
         .tvOS(.v17),
-        .watchOS(.v10)
+        .watchOS(.v10),
+        .visionOS(.v1)
     ],
     products: [
         .library(
@@ -21,7 +22,7 @@ let package = Package(
         .package(url: "https://github.com/danielsaidi/BadgeIcon.git", .upToNextMinor(from: "0.6.0")),
         .package(url: "https://github.com/simibac/ConfettiSwiftUI.git", .upToNextMajor(from: "1.1.0")),
         .package(url: "https://github.com/danielsaidi/OnboardingKit.git", .upToNextMajor(from: "8.3.0")),
-        .package(url: "https://github.com/danielsaidi/StandardButtons.git", .upToNextMinor(from: "0.1.0")),
+        .package(url: "https://github.com/danielsaidi/StandardButtons.git", .upToNextMajor(from: "0.3.0")),
         .package(url: "https://github.com/danielsaidi/StoreKitPlus.git", .upToNextMinor(from: "0.7.0")),
         .package(url: "https://github.com/danielsaidi/SwiftUIKit.git", .upToNextMajor(from: "5.1.1")),
     ],
@@ -32,8 +33,9 @@ let package = Package(
                 "BadgeIcon",
                 "ConfettiSwiftUI",
                 "OnboardingKit",
-                "SwiftUIKit",
-                "StoreKitPlus"
+                "StandardButtons",
+                "StoreKitPlus",
+                "SwiftUIKit"
             ],
             resources: [.process("Resources")]
         ),

5169c967921d4ee27ef2f4226b108d1675f78835
Daniel Saidi <daniel.saidi@gmail.com>
2025-07-16 10:34:18 +0200

    Adjust about screen content to fix strange header anomaly

--- Sources/KankodaKit/About/AboutScreenContent.swift
+++ Sources/KankodaKit/About/AboutScreenContent.swift
@@ -30,12 +30,14 @@ public struct AboutScreenContent: View {
     public var body: some View {
         List {
             ListHeader {
-                icon
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(maxWidth: 150, maxHeight: 150)
-                    .padding(.vertical, 30)
-                description
+                VStack {
+                    icon
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: 150, maxHeight: 150)
+                        .padding(.vertical, 30)
+                    description
+                }
             }
             .listRowInsets(.init(horizontal: 0, vertical: 10))
 
@@ -73,12 +75,12 @@ private extension AboutScreenContent {
 }
 
 #Preview {
-    AboutScreenContent(
-        .preview,
-        icon: .lightbulb,
-        description: Text("This app is developed by Kankoda in Stockholm, Sweden.\n\nDon't hesitate to reach out if you find bugs, have ideas on how to improve this app, or just want to say hi!")
-    )
-    .appInfoMenuItemStyle(.badge)
-    .background(Color.red)
-    .frame(height: 800)
+    NavigationStack {
+        AboutScreenContent(
+            .preview,
+            icon: .lightbulb,
+            description: Text("This app is developed by Kankoda in Stockholm, Sweden.\n\nDon't hesitate to reach out if you find bugs, or have ideas on how to improve the app.")
+        )
+        .appInfoMenuItemStyle(.plain)
+    }
 }
